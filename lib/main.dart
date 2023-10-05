a6c5d6ed6b7f50ad44a1c308c6345ca34f802890
Asif <asif809999@gmail.com>
2023-10-05 08:23:09 +0600

    meals data checked into screen 8.8

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,7 +23,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: //const CategoriesScreen(),
+          const MealsScreen(
+        title: 'Some category',
+        meals: dummyMeals,
+      ),
     );
   }
 }
